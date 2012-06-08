427b3e13d6968904522f2e8edbe6de49d979036b
Paul Schwarz <paulsschwarz@gmail.com>
2012-06-08 17:50:22 +0300

    Removed DI double standards by forcing $select to be injected.

--- application/classes/automodeler/gateway/groups.php
+++ application/classes/automodeler/gateway/groups.php
@@ -5,22 +5,18 @@ class AutoModeler_Gateway_Groups extends AutoModeler_Gateway_Database
 	protected $_model_name = 'Model_Group';
 	protected $_table_name = 'groups';
 
-	public function find_groups(Database_Query_Builder_Select $select = NULL)
+	public function find_groups(Database_Query_Builder_Select $select)
 	{
 		return $this->_load_set($select);
 	}
 
-	public function find_group($id, Database_Query_Builder_Select $select = NULL)
+	public function find_group($id, Database_Query_Builder_Select $select)
 	{
-		if ($select === NULL)
-		{
-			$select = db::select();
-		}
-
 		$select->where('id', '=', $id);
 		return $this->_load_object($select);
 	}
 
+	// TODO: convert to proper DI
 	public function assign_groups_to_user($user_id, array $groups, Database_Query_Builder_Insert $insert = NULL)
 	{
 		if ( ! empty($groups))
