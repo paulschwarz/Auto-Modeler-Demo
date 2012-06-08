a9f1aae71c1b6dcee65609534c149e005dcfda84
Paul Schwarz <paulsschwarz@gmail.com>
2012-06-08 17:51:05 +0300

    Removed DI double standards by forcing $select to be injected.

--- application/classes/automodeler/gateway/users.php
+++ application/classes/automodeler/gateway/users.php
@@ -5,18 +5,13 @@ class AutoModeler_Gateway_Users extends AutoModeler_Gateway_Database
 	protected $_model_name = 'Model_User';
 	protected $_table_name = 'users';
 
-	public function find_users(Database_Query_Builder_Select $select = NULL)
+	public function find_users(Database_Query_Builder_Select $select)
 	{
 		return $this->_load_set($select);
 	}
 
-	public function find_user($id, Database_Query_Builder_Select $select = NULL)
+	public function find_user($id, Database_Query_Builder_Select $select)
 	{
-		if ($select === NULL)
-		{
-			$select = db::select();
-		}
-
 		$select->where('id', '=', $id);
 		return $this->_load_object($select);
 	}
